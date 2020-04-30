e14e9e7189124205fbd8f0483561886e9c90cdd5
hisarcode <hisarcode@gmail.com>
2020-04-30 16:33:32 +0700

    Edit Menu.php Edit Menu_m Menambah fungsi submenu di Controller Menu

--- application/views/menu/submenu.php
+++ application/views/menu/submenu.php
@@ -5,19 +5,24 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
     <div class="row">
-        <div class="col-lg-6">
-            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
-            <?php if ($this->session->flashdata('category_error')) : ?>
-                <div class="alert alert-danger" role="alert"> <?= $this->session->flashdata('category_error') ?> </div>
+        <div class="col-lg">
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= validation_errors(); ?>
+                </div>
             <?php endif; ?>
 
-            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahSubmenuModal">Tambah Submenu Baru</a>
+            <?php if ($this->session->flashdata('category_success')) : ?>
+                <div class="alert alert-success" role="alert"> <?= $this->session->flashdata('category_success') ?> </div>
+            <?php endif; ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahSubMenuModal">Tambah Submenu Baru</a>
 
             <table class="table table-hover">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Sub Menu</th>
+                        <th scope="col">Title</th>
                         <th scope="col">Menu</th>
                         <th scope="col">Url</th>
                         <th scope="col">Icon</th>
@@ -28,10 +33,14 @@
                 </thead>
                 <tbody>
                     <?php $i = 1; ?>
-                    <?php foreach ($menu as $m) : ?>
+                    <?php foreach ($subMenu as $sm) : ?>
                         <tr>
                             <th scope="row"><?= $i; ?></th>
-                            <td><?= $m['menu']; ?></td>
+                            <td><?= $sm['title']; ?></td>
+                            <td><?= $sm['menu']; ?></td>
+                            <td><?= $sm['url']; ?></td>
+                            <td><?= $sm['icon']; ?></td>
+                            <td><?= $sm['is_active']; ?></td>
                             <td>
                                 <a href="" class="badge badge-success">Edit</a>
                                 <a href="" class="badge badge-danger">Delete</a>
@@ -54,20 +63,45 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="tambahMenuModal" tabindex="-1" role="dialog" aria-labelledby="tambahMenuModalLabel" aria-hidden="true">
+<div class="modal fade" id="tambahSubMenuModal" tabindex="-1" role="dialog" aria-labelledby="tambahSubMenuModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="tambahMenuModalLabel">Tambah Menu Baru</h5>
+                <h5 class="modal-title" id="tambahSubMenuModalLabel">Tambah Sub Menu Baru</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?= base_url('menu'); ?>" method="POST">
+            <form action="<?= base_url('menu/submenu'); ?>" method="POST">
                 <div class="modal-body">
                     <div class="form-group">
-                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Nama Menu">
+                        <input type="text" class="form-control" id="title" name="title" placeholder="Sub Menu Title">
                     </div>
+
+                    <div class="form-group">
+                        <select name="menu_id" id="menu_id" class="form-control">
+                            <option value="">Pilih Menu</option>
+                            <?php foreach ($menu as $m) : ?>
+                                <option value="<?= $m['id']; ?>"><?= $m['menu']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="url" name="url" placeholder="Sub Menu Url">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="icon" name="icon" placeholder="Sub Menu Icon">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" value="1" name="is_active" id="is_active" checked>
+                            <label for="is_active" class="form-check-label">Active</label>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
