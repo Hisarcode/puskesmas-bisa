a3411907ee1ab0fec122f7e23383994698f44265
hisarcode <hisarcode@gmail.com>
2020-05-01 06:22:47 +0700

    Tambah model menu

--- application/models/Menu_m.php
+++ application/models/Menu_m.php
@@ -7,7 +7,7 @@ class Menu_m extends CI_Model
     {
         $query = "SELECT `user_sub_menu`.*, `user_menu`.`menu`
                 FROM `user_sub_menu` JOIN `user_menu`
-                ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                ON `user_sub_menu`.`menu_id` = `user_menu`.`id` ORDER BY `user_sub_menu`.`menu_id` ASC
         ";
         return $this->db->query($query)->result_array();
     }
