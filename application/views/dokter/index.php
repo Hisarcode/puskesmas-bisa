027c51234f6ac92f27bc7e3ab23eebd103f53b7c
hisarcode <hisarcode@gmail.com>
2020-05-01 06:20:53 +0700

    tambah construct dan fix  view dokter

--- application/views/dokter/index.php
+++ application/views/dokter/index.php
@@ -4,7 +4,22 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img class="card-img" src="<?= base_url('assets/img/profile/') . $user['image']; ?>">
+            </div>
 
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $user['nama']; ?></h5>
+                    <p class="card-text"><?= $user['nik']; ?></p>
+                    <p class="card-text"><?= $user['alamat']; ?></p>
+                    <p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']); ?></small></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- /.container-fluid -->
 
