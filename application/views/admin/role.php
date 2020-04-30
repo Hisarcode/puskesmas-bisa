2298d6777e95b9833758da16ce83dc4f0a7c8bca
hisarcode <hisarcode@gmail.com>
2020-05-01 06:14:45 +0700

    tambahan di view Admin untuk role

---
+++ application/views/admin/role.php
@@ -0,0 +1,75 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?php if ($this->session->flashdata('category_error')) : ?>
+                <div class="alert alert-danger" role="alert"> <?= $this->session->flashdata('category_error') ?> </div>
+            <?php endif; ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahRoleModal">Tambah Role Baru</a>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($role as $r) : ?>
+                        <tr>
+                            <th scope="row"><?= $i; ?></th>
+                            <td><?= $r['role']; ?></td>
+                            <td>
+                                <a href="<?= base_url('admin/roleaccess/') . $r['id']; ?>" class="badge badge-warning">Akses</a>
+                                <a href="" class="badge badge-success">Edit</a>
+                                <a href="" class="badge badge-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <?php $i++;  ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="tambahRoleModal" tabindex="-1" role="dialog" aria-labelledby="tambahRoleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tambahRoleModalLabel">Tambah Role Baru</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('admin/role'); ?>" method="POST">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="role" name="role" placeholder="Nama Role">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
