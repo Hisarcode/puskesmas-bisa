b6912a24771238ccb78c239a7a94bc19067c4ac8
hisarcode <hisarcode@gmail.com>
2020-05-08 04:27:53 +0700

    add fungsi edit-delete di co Menu untuk Menu&Sub

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -33,6 +33,41 @@ class Menu extends CI_Controller
         }
     }
 
+    public function getEditMenu()
+    {
+        $this->load->model('Menu_m', 'menu');
+
+        echo json_encode($this->menu->getMenuById($_POST['id']));
+    }
+
+    public function editMenu()
+    {
+        $this->load->model('Menu_m', 'menu');
+        if ($this->menu->editDataMenu($_POST) > 0) {
+            $this->session->set_flashdata('category_success', 'Menu Telah Diedit');
+            redirect('menu');
+            exit;
+        } else {
+            $this->session->set_flashdata('category_error', 'Menu Gagal Diedit');
+            redirect('menu');
+            exit;
+        }
+    }
+
+    public function deleteMenu($id)
+    {
+        $this->load->model('Menu_m', 'menu');
+        if ($this->menu->deleteDataMenu($id) > 0) {
+            $this->session->set_flashdata('category_success', 'Menu Telah Dihapus');
+            redirect('menu');
+            exit;
+        } else {
+            $this->session->set_flashdata('category_error', 'Menu Gagal Dihapus');
+            redirect('menu');
+            exit;
+        }
+    }
+
     public function submenu()
     {
         $data['title'] = "Manajemen Submenu";
@@ -40,14 +75,13 @@ class Menu extends CI_Controller
         $this->load->model('Menu_m', 'menu');
 
         $data['menu'] = $this->db->get('user_menu')->result_array();
-
+        // validasi input modal
         $this->form_validation->set_rules('title', 'Title', 'required');
         $this->form_validation->set_rules('menu_id', 'Menu', 'required');
         $this->form_validation->set_rules('url', 'URL', 'required');
         $this->form_validation->set_rules('icon', 'Icon', 'required');
 
         if ($this->form_validation->run() == false) {
-
             $data['subMenu'] = $this->menu->getSubMenu();
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
@@ -55,17 +89,49 @@ class Menu extends CI_Controller
             $this->load->view('menu/submenu', $data);
             $this->load->view('templates/footer', $data);
         } else {
-            $data = [
-                'title' =>  $this->input->post('title'),
-                'menu_id' =>  $this->input->post('menu_id'),
-                'url' =>  $this->input->post('url'),
-                'icon' =>  $this->input->post('icon'),
-                'is_active' =>  $this->input->post('is_active')
-            ];
+            if ($this->menu->insertDataSubMenu() > 0) {
+                $this->session->set_flashdata('category_success', 'Sub Menu Telah Ditambahkan');
+                redirect('menu/submenu');
+                exit;
+            } else {
+                $this->session->set_flashdata('category_error', 'Sub Menu Gagal Ditambahkan');
+                redirect('menu/submenu');
+                exit;
+            }
+        }
+    }
 
-            $this->db->insert('user_sub_menu', $data);
-            $this->session->set_flashdata('category_success', 'Sub Menu Telah Ditambahkan');
+    public function getEditSubMenu()
+    {
+        $this->load->model('Menu_m', 'menu');
+        echo json_encode($this->menu->getSubMenuById($_POST['id']));
+    }
+
+    public function editSubMenu()
+    {
+        $this->load->model('Menu_m', 'menu');
+        if ($this->menu->editDataSubMenu($_POST) > 0) {
+            $this->session->set_flashdata('category_success', 'Sub Menu Telah Diedit');
             redirect('menu/submenu');
+            exit;
+        } else {
+            $this->session->set_flashdata('category_error', 'Sub Menu Gagal Diedit');
+            redirect('menu/submenu');
+            exit;
+        }
+    }
+
+    public function deleteSubMenu($id)
+    {
+        $this->load->model('Menu_m', 'menu');
+        if ($this->menu->deleteDataSubMenu($id) > 0) {
+            $this->session->set_flashdata('category_success', 'Sub Menu Telah Dihapus');
+            redirect('menu/submenu');
+            exit;
+        } else {
+            $this->session->set_flashdata('category_error', 'Sub Menu Gagal Dihapus');
+            redirect('menu/submenu');
+            exit;
         }
     }
 }
