a7c99ea7f0c00e2ecf6c0a52bd97edc75c79833f
hisarcode <hisarcode@gmail.com>
2020-05-08 04:28:47 +0700

    Membuat controller-view Surat Rujukan

---
+++ application/controllers/Surat_Rujukan.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Surat_Rujukan extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function inddex()
+    {
+        $data['title'] = "Surat Rujukan";
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+
+        //bisa hubungkan ke model
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($this->form_validation->run() == false) {
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('suratrujukan/index', $data);
+            $this->load->view('templates/footer', $data);
+        } else {
+            $this->db->insert('user_menu',  ['menu' => $this->input->post('menu')]);
+            $this->session->set_flashdata('category_success', 'Menu Telah Ditambahkan');
+            redirect('menu');
+        }
+    }
+}
