54e9b0a8fac743ebbacdfb252b281b01e5f92653
Hisarcode <hisarcode@gmail.com>
2020-06-01 16:27:21 +0700

    edit src untuk tanggal

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -54,6 +54,10 @@
 <script src="<?= base_url(); ?>assets/js/popper.min.js"></script>
 <script src="<?= base_url(); ?>assets/js/script.js"></script>
 <script src="<?= base_url(); ?>assets/js/script_obat.js"></script>
+<link rel="stylesheet" href="<?= base_url('assets/'); ?>plugins/jquery-ui/jquery-ui.css">
+
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 
 <script>
@@ -76,6 +80,18 @@
     })
 </script>
 
+<script>
+    $(function() {
+        $("#tanggalantrian").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: '1950:2020',
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            todayHighlight: true,
+        });
+    });
+</script>
 </body>
 
 </html>
