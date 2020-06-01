daf98205a4c5d49479bb93d8e5ab6239b89e4e4c
dinandadp <dinandadepaguita@gmail.com>
2020-06-01 14:41:19 +0700

    edit untuk keperluan pagination

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -4,14 +4,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class User_m extends CI_Model
 {
     //untuk data pasien
-    public function tampilDataPasien($limit, $start='0'){
+    public function tampilDataPasien($limit, $start = '0')
+    {
         $query = "SELECT `pasien`.*, `user`.`username`, `user`.`nama`, `user`.`alamat`
                 FROM `pasien` JOIN `user`
-                ON `pasien`.`user_id` = `user`.`id` ORDER BY `pasien`.`user_id` ASC LIMIT ". $start.",".$limit;
-       return $this->db->query($query)->result_array();
+                ON `pasien`.`user_id` = `user`.`id` ORDER BY `pasien`.`user_id` ASC LIMIT " . $start . "," . $limit;
+        return $this->db->query($query)->result_array();
     }
 
-    public function countSemuaPasien(){
+    public function countSemuaPasien()
+    {
         return $this->db->get('pasien')->num_rows();
     }
 
@@ -19,40 +21,62 @@ class User_m extends CI_Model
     {
         $query = "SELECT `pasien`.*, `user`.`username`, `user`.`nama`, `user`.`alamat`
         FROM `pasien` JOIN `user`
-        ON `pasien`.`user_id` = `user`.`id` WHERE `user`.`username` OR `user`.`nama`  LIKE '%".$cari."%' ORDER BY `pasien`.`user_id` ASC";
+        ON `pasien`.`user_id` = `user`.`id` WHERE `user`.`username` OR `user`.`nama`  LIKE '%" . $cari . "%' ORDER BY `pasien`.`user_id` ASC";
 
         return $this->db->query($query)->result_array();
+    }
+
+    public function getPasienId($userId)
+    {
+        return $this->db->get_where('pasien', array('user_id' => $userId))->row()->id;
     }
 
     //untuk data resep
-    public function tampilDataResep($limit, $start='0'){
-        $query = "SELECT 
-                `resep`.*, `dokter`.`nama_gelar`, `dokter`.`jenis_dokter`,
-                `dokter`.*, `user`.`nama`
+    public function tampilDataResep($limit, $start = '0', $idPasien)
+    {
+        $query = "SELECT *
                 FROM `resep` 
                 JOIN `dokter`ON `resep`.`dokter_id` = `dokter`.`id` 
-                JOIN `user` ON `dokter`.`user_id` = `user`.`id`
-                ORDER BY `resep`.`date_created` ASC LIMIT ". $start.",".$limit;
+                WHERE `pasien_id` ='" . $idPasien . "' 
+                GROUP BY `resep`.`date_created` ORDER BY `resep`.`id` ASC LIMIT " . $start . "," . $limit;
 
+        //return $this->db->query('user', ['username' => $this->session->userdata('username')])->row_array();
         return $this->db->query($query)->result_array();
+        //$data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
     }
 
-    public function countSemuaResep(){
+    public function countSemuaResep()
+    {
         return $this->db->get('resep')->num_rows();
     }
 
-    public function tampilDataResepCari($cari)
+    public function detail_resep($hari)
     {
-        $query = "SELECT 
-        `resep`.*, `dokter`.`nama_gelar`, `dokter`.`jenis_dokter`,
-        `dokter`.*, `user`.`nama`
-        FROM `resep` 
-        JOIN `dokter`ON `resep`.`dokter_id` = `dokter`.`id` 
-        JOIN `user` ON `dokter`.`user_id` = `user`.`id`
-        WHERE `resep`.`date_created` LIKE '%".$cari."%' 
-        ORDER BY `resep`.`date_created` ASC";
 
+        $query = "SELECT *
+        FROM `resep` 
+        JOIN `obat` ON `resep`.`obat_id` = `obat`.`id` 
+        WHERE `resep`.`date_created` ='" . $hari . "'";
         return $this->db->query($query)->result_array();
+    }
+
+    //untuk data surat rujukan
+    public function tampilDataRujukan($limit, $start = '0', $idPasien)
+    {
+        $query = "SELECT *
+                FROM `surat_rujukan` 
+                JOIN `dokter`ON `surat_rujukan`.`dokter_id` = `dokter`.`id` 
+                WHERE `pasien_id` ='" . $idPasien . "' 
+                ORDER BY `surat_rujukan`.`id` ASC LIMIT " . $start . "," . $limit;
+
+        //return $this->db->query('user', ['username' => $this->session->userdata('username')])->row_array();
+        return $this->db->query($query)->result_array();
+        //$data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+    }
+
+    public function countSemuaRujukan()
+    {
+        return $this->db->get('surat_rujukan')->num_rows();
     }
 
     public function detail_pasien($id = NULL)
@@ -60,7 +84,7 @@ class User_m extends CI_Model
         $query = "SELECT `pasien`.*, `user`.`nama`, `user`.`nik`, `user`.`tanggallahir`,`user`.`alamat`,`user`.`email`
         FROM `pasien` 
         JOIN `user` ON `pasien`.`user_id` = `user`.`id` 
-        WHERE `pasien`.`id` =".$id;
+        WHERE `pasien`.`id` =" . $id;
 
         return $this->db->query($query)->row_array();
     }
