3484a2b194969549d19abc2b89eb12c193b56d03
hisarcode <hisarcode@gmail.com>
2020-06-18 13:12:59 +0700

    fix dokter controller

--- application/controllers/Dokter.php
+++ application/controllers/Dokter.php
@@ -8,7 +8,6 @@ class Dokter extends CI_Controller
         parent::__construct();
         $this->load->library('session');
         $this->load->model('User_m', 'user');
-        $this->load->model(array('User_m'));
         $this->load->library('pagination');
         is_logged_in();
     }
@@ -27,59 +26,33 @@ class Dokter extends CI_Controller
 
     public function datapasien()
     {
-        $data['cari'] = $this->input->post('cari', true);
-        $data['start'] = $this->uri->segment(3);
-        $data['tampildata'] = '';
-
-        // cek apakah data kosong
-        if (isset($data['cari']) && !empty($data['cari'])) {
-            // kalau tidak cari data sesuai kata cari
-            $data['tampildata'] = $this->user->tampilDataPasienCari($data['cari']);
-        } else {
-            $config['base_url'] = 'http://localhost/puskesmas-bisa/dokter/datapasien';
-            $config['total_rows'] = $this->user->countSemuaPasien();
-            $config['per_page'] = 10;
-
-            // styling pagination
-            $config['full_tag_open'] = '<nav><ul class="pagination justify-content-center">';
-            $config['full_tag_close'] = '</ul></nav>';
-
-            $config['first_link'] = 'First';
-            $config['first_tag_open'] = '<li class="page-item">';
-            $config['first_tag_close'] = '</li>';
-
-            $config['last_link'] = 'Last';
-            $config['last_tag_open'] = '<li class="page-item">';
-            $config['last_tag_close'] = '</li>';
-
-            $config['next_link'] = '&raquo';
-            $config['next_tag_open'] = '<li class="page-item">';
-            $config['next_tag_close'] = '</li>';
-
-            $config['prev_link'] = '&laquo';
-            $config['prev_tag_open'] = '<li class="page-item">';
-            $config['prev_tag_close'] = '</li>';
-
-            $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
-            $config['cur_tag_close'] = '</a></li>';
-
-            $config['num_tag_open'] = '<li class="page-item">';
-            $config['num_tag_close'] = '</li>';
-
-            $config['attributes'] = array('class' => 'page-link');
-
-            // initialize pagination
-            $this->pagination->initialize($config);
-            $data['start'] = $this->uri->segment(3);
-            if (empty($data['start'])) {
-                $data['start'] = 0;
-            }
-            //tampilkan semua data pasien per page
-            $data['tampildata'] = $this->user->tampilDataPasien($config['per_page'], $data['start']);
-        }
-
         $data['title'] = "Data Pasien";
         $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+
+        // ambil data cari
+        if ($this->input->post('tombolcari')) {
+            $data['cari'] = $this->input->post('cari');
+            $this->session->set_userdata('caridatapasien', $data['cari']);
+        } else {
+            $data['cari'] = $this->session->userdata('caridatapasien');
+        }
+
+        $config['base_url'] = base_url('dokter/datapasien');
+        $this->db->select('*');
+        $this->db->from('pasien');
+        $this->db->join('user', 'user.id = pasien.user_id');
+        $this->db->like('nama', $data['cari']);
+        $config['total_rows'] = $this->db->count_all_results();
+        $data['total_rows'] = $config['total_rows'];
+        $config['per_page'] = 3;
+
+
+        // initialize pagination
+        $this->pagination->initialize($config);
+        $data['start'] = $this->uri->segment(3);
+
+        //tampilkan semua data pasien per page
+        $data['tampildata'] = $this->user->tampilDataPasien($config['per_page'], $data['start'], $data['cari']);
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
@@ -88,10 +61,12 @@ class Dokter extends CI_Controller
         $this->load->view('templates/footer', $data);
     }
 
+
+
     //detail data
     public function detail($id)
     {
-        $data['detail'] = $this->User_m->detail_pasien($id);
+        $data['detail'] = $this->user->detail_pasien($id);
 
         $data['title'] = "Detail Data Pasien";
         $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
