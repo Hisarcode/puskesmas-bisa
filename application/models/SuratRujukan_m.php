26770118d14eaf873309d01400051c6ec23bb330
hisarcode <hisarcode@gmail.com>
2020-06-18 13:09:14 +0700

    add suratrujukan_m

---
+++ application/models/SuratRujukan_m.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class SuratRujukan_m extends CI_Model
+{
+
+    public function getSuratRujukanById($id)
+    {
+        return  $this->db->get_where('surat_rujukan', ['id' => $id])->row_array();
+    }
+
+
+    public function deleteDataSuratRujukan($id)
+    {
+        $this->db->delete('surat_rujukan', array('id' => $id));
+        return $this->db->affected_rows();
+    }
+
+    public function editDataSuratRujukan($data)
+    {
+        $this->db->where('id', $data['id']);
+        //ganti dengan isi dari suratrujukan yang baru
+        $this->db->update('surat_rujukan', array(
+            'menu' => $data['menu'],
+        ));
+        return $this->db->affected_rows();
+    }
+}
